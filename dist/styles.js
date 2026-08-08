94cbfcfc227152424afb25742a7adfd4ac305240
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-08 13:07:44 +0100

    Moved SCSS pages around locations

--- dist/styles.js
+++ dist/styles.js
@@ -1,28 +1,25 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	const __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-let __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/scss/main.scss"
 /*!*******************************!*\
   !*** ./assets/scss/main.scss ***!
   \*******************************/
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
+() {
 
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nError: Execution of module code from module graph (./assets/scss/main.scss.webpack[javascript/auto]!=!./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./assets/scss/main.scss) failed: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n   ╷\n41 │ @import './layout/footer.scss';\n   │         ^^^^^^^^^^^^^^^^^^^^^^\n   ╵\n  assets/scss/main.scss 41:9  root stylesheet");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	let __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./assets/scss/main.scss"]();
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=styles.js.map
