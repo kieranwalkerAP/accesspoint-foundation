9fd587fc285a0f35f0fc088741108dd51c193ce9
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-10 16:53:06 +0100

    Refactor text-image section structure and styles for improved layout and responsiveness

--- page-sections/sections/text_image.php
+++ page-sections/sections/text_image.php
@@ -1,49 +1,46 @@
 <?php 
 
   $background = get_sub_field ('background_colour');
-  $alignment = get_sub_field('allignment');
   $image = get_sub_field('image');
   $content = get_sub_field('content');
   $link = get_sub_field('link');
   $title = get_sub_field('title');
   $image = get_sub_field('image');
-  $imagePosition = get_sub_field('image_left');
-  $padding = get_sub_field('padding_size');
+  $imagePosition = get_sub_field('image_position');
+  $button_class = $background === 'primary-colour' ? 'white' : ($background === 'secondary-colour' ? 'dark' : 'primary-colour');
 
 ?>
 
-<section class="text-image background-<?php echo $background ?> padding-<?php echo $padding ?>">
-  <div class="container">
-    <div class="row justify-content-between <?php echo esc_attr($alignment); ?> <?php echo $imagePosition ? 'orientation-left' : 'orientation-right'; ?>">
+<section class="text-image background-<?php echo $background ?>">
+  <div class="text-image__inner container <?php echo $imagePosition ? 'orientation-left' : 'orientation-right'; ?>">
         <?php if ( $image ) : ?>
-          <div class="col-12 col-lg-6">
-            <div class="text-image__image">
-              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="img-fluid image"/>
-            </div>
+          <div class="text-image__inner--image">
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="image"/>
           </div>
         <?php endif; ?>
         <?php if ($title or $content) : ?>
-          <div class="<?php echo !empty($image) ? 'col-12 col-lg-5' : 'col-12' ;?>">
-            <div class="text-image__content">
+
+            <div class="text-image__inner--content">
               <?php if ($title): ?>
-                <h2><?php echo $title ?></h2>
+                <h2 class="text-image__inner--title"><?php echo $title ?></h2>
               <?php endif ; ?>
+
               <?php if ($content or $link ): ?>
-                <div class="text-image__content--copy">
+                <div class="text-image__inner--copy wys-reset">
                   <?php echo $content ?>
+
                   <?php if( $link ):
                     $link_url = $link['url'];
                     $link_title = $link['title'];
                     $link_target = $link['target'] ? $link['target'] : '_self';
                   ?>
-                    <a class="button button-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                <?php endif; ?>
+                      <a class="button button-<?php echo($button_class)?>" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                  <?php endif; ?>
+                  
                 </div>
               <?php endif; ?>
             </div>
-          </div>
         <?php endif; ?>
       </div>
     </div>
-  </div>
 </section>
